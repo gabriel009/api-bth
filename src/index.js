690d339e02477dd909abfcd2337211f747f2cf15
Gabriel Nascimento <gabriel.nascimento3110@outlook.com>
2020-04-18 15:51:35 -0300

    Update host Server

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const routes = require('./routes')
 
 const app = express();
 
-app.use(cors( /*{origin: 'http://meuapp.com'}*/ ))
+app.use(cors( {origin: 'https://bth-api-nascimento.herokuapp.com/'} ))
 app.use(express.json());
 app.use(routes);
 
